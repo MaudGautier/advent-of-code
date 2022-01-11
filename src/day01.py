d1dc850e1845e29631bdb86b69c8bd981650a291
Maud <maud@ovrsea.com>
2022-01-11 21:56:23 +0100

    feat(day01): add 1-b solution

--- src/day01.py
+++ src/day01.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, List
 
 
 def has_depth_increased(previous_depth: Union[int, type(None)], new_depth: int) -> bool:
@@ -7,20 +7,30 @@ def has_depth_increased(previous_depth: Union[int, type(None)], new_depth: int) 
     return new_depth > previous_depth
 
 
-def count_nb_increases(input_file: str) -> int:
+def sum_depths(previous_n_depth: List[int], depth: int) -> Union[int, type(None)]:
+    if None in previous_n_depth:
+        return None
+    return sum(previous_n_depth, depth)
+
+
+def count_nb_increases(input_file: str, window_size: int) -> int:
     total_increases = 0
-    previous_depth = None
+    previous_depth_sum = None
+    previous_n_depths = [None]*(window_size-1)
     with open(input_file) as file:
         lines = file.readlines()
         for line in lines:
             depth = int(line.strip())
-            if has_depth_increased(previous_depth, depth):
+            depth_sum = sum_depths(previous_n_depths, depth)
+            if has_depth_increased(previous_depth_sum, depth_sum):
                 total_increases += 1
-            previous_depth = depth
+            previous_n_depths = (previous_n_depths + [depth])[1:]
+            previous_depth_sum = depth_sum
 
     return total_increases
 
 
 if __name__ == "__main__":
-    print(count_nb_increases("data/day01-input.txt"))
+    print(count_nb_increases("data/day01-input.txt", 1) == 1184) # should still be 1184
+    print(count_nb_increases("data/day01-input.txt", 3)) # 1158
 
