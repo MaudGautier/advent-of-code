eeddb0d815ae4c581cc3ca9c964740d1dad03455
Maud <maud@ovrsea.com>
2022-01-24 21:32:52 +0100

    feat(day07): add 7-b solution

--- src/day07.py
+++ src/day07.py
@@ -6,17 +6,39 @@ Positions = List[int]
 FuelAmount = int
 
 
-def calculate_fuel_amount_to_reach_position(
+def calculate_fuel_amount_to_reach_target(
     initial_positions: Positions, target: Position
 ) -> FuelAmount:
     fuel_amounts = [abs(target - p) for p in initial_positions]
     return sum(fuel_amounts)
 
 
-def get_min_fuel(crab_positions: Positions) -> FuelAmount:
+def fancy_sum(max_value: int, min_value: int = 0) -> int:
+    total = 0
+    incrementor = 0
+    for lap in range(min_value, max_value + 1):
+        total += incrementor
+        incrementor += 1
+
+    return total
+
+
+def calculate_non_linear_fuel_amount_to_reach_target(
+    initial_positions: Positions, target: Position
+) -> FuelAmount:
+    fuel_amounts = [fancy_sum(abs(target - p)) for p in initial_positions]
+    return sum(fuel_amounts)
+
+
+def get_min_fuel(crab_positions: Positions, non_linear: bool = False) -> FuelAmount:
     fuel_amounts = []
     for target in range(min(crab_positions), max(crab_positions) + 1):
-        fuel_amount = calculate_fuel_amount_to_reach_position(crab_positions, target)
+        if non_linear:
+            fuel_amount = calculate_non_linear_fuel_amount_to_reach_target(
+                crab_positions, target
+            )
+        else:
+            fuel_amount = calculate_fuel_amount_to_reach_target(crab_positions, target)
         fuel_amounts.append(fuel_amount)
 
     return min(fuel_amounts)
@@ -33,9 +55,20 @@ if __name__ == "__main__":
     test_crab_positions = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
     test_minimal_fuel = get_min_fuel(test_crab_positions)
     print(test_minimal_fuel == 37)
+    test_minimal_fuel_non_linear = get_min_fuel(test_crab_positions, non_linear=True)
+    print(test_minimal_fuel_non_linear == 168)
 
     # Solution for 7-a
     print("\n-- Solution for 7-a:")
     crab_positions = read_data("data/day07-input.txt")
     minimal_fuel = get_min_fuel(crab_positions)
     print("The minimal fuel amount for the selected position is:", minimal_fuel)
+
+    # Solution for 7-b
+    # TODO: improve perf
+    print("\n-- Solution for 7-b:")
+    minimal_fuel_non_linear = get_min_fuel(crab_positions, non_linear=True)
+    print(
+        "The minimal fuel amount with non-linear calculus method for the selected position is:",
+        minimal_fuel_non_linear,
+    )  # 96708205
