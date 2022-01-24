e96feac4682d6a4e1bd6344d5aa55cbade3c1158
Maud <maud@ovrsea.com>
2022-01-24 20:53:32 +0100

    feat(day06): add 6-b solution

--- src/day06.py
+++ src/day06.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-from typing import List, NamedTuple
+from typing import List, Dict
 
-Timers = List[int]
+Timer = int
+Timers = List[Timer]
+Count = int
+TimerCounter = Dict[Timer, Count]
 
 
 def initialize_fish_timers(file_name: str) -> Timers:
@@ -10,25 +13,30 @@ def initialize_fish_timers(file_name: str) -> Timers:
         return [int(timer) for timer in line.split(",")]
 
 
-def update_timers(timers: Timers):
-    nb_new_timers = 0
-    for timer_index, timer in enumerate(timers):
-        if timer == 0:
-            timers[timer_index] = 6
-            nb_new_timers += 1
-        else:
-            timers[timer_index] -= 1
+def get_timer_counter(timers: Timers) -> TimerCounter:
+    timer_counts = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
+    for timer in timers:
+        timer_counts[timer] += 1
 
-    timers.extend([8] * nb_new_timers)
+    return timer_counts
+
+
+def update_timer_counter(timer_counter: TimerCounter):
+    nb_fishes_to_add = timer_counter[0]
+    for timer in range(1, 9):
+        timer_counter[timer - 1] = timer_counter[timer]
+    timer_counter[8] = nb_fishes_to_add
+    timer_counter[6] = timer_counter[6] + nb_fishes_to_add
+
+    return timer_counter
 
 
 def count_fish(timers: Timers, nb_days: int) -> int:
-    # Only copy list of timers to avoid mistakes if reusing the same input list on several tests
-    timers = timers.copy()
-    for day in range(nb_days):
-        update_timers(timers)
+    timer_counter = get_timer_counter(timers)
+    for day in range(1, nb_days + 1):
+        update_timer_counter(timer_counter)
 
-    return len(timers)
+    return sum(timer_counter.values())
 
 
 if __name__ == "__main__":
@@ -39,9 +47,16 @@ if __name__ == "__main__":
     print(nb_test_fishes_after_18_days == 26)
     nb_test_fishes_after_80_days = count_fish(test_initial_fish_timers, 80)
     print(nb_test_fishes_after_80_days == 5934)
+    nb_test_fishes_after_256_days = count_fish(test_initial_fish_timers, 256)
+    print(nb_test_fishes_after_256_days == 26984457539)
 
     # Solution for 6-a
     print("\n-- Solution for 6-a:")
     initial_fish_timers = initialize_fish_timers("data/day06-input.txt")
     nb_fishes_after_80_days = count_fish(initial_fish_timers, 80)
     print("After 80 days, there are", nb_fishes_after_80_days, "fishes")  # 380243
+
+    # Solution for 6-b
+    print("\n-- Solution for 6-b:")
+    nb_fishes_after_256_days = count_fish(initial_fish_timers, 256)
+    print("After 256 days, there are", nb_fishes_after_256_days, "fishes")
