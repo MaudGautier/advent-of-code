8bfc6a8fefc5a3a28f0fc4b115ff0851ffa3f552
Maud <maud.j.gautier@gmail.com>
2022-12-01 19:53:47 +0100

    feat(2022): add 1-b solution (+ refactor)

--- 2022/src/day01.py
+++ 2022/src/day01.py
@@ -2,34 +2,69 @@
 from typing import Union, List
 
 
-# Output: list of bag calories: [2000, 3000, 1000]
-def extract_elves_bags(input_file: str) -> List[int]:
-    with open(input_file) as file:
-        lines = file.readlines()
-        elves_bags = []
-        current_bag = 0
-        for line in lines:
-            if line == "\n":
-                elves_bags.append(current_bag)
-                current_bag = 0
-            else:
-                current_bag += int(line.strip())
-    return elves_bags
+# Data is formated as: ["4887", "9307", "", "5533", "7981", ...]
+def read_data(file_name: str) -> List[str]:
+    with open(file_name, "r") as file:
+        return [line.strip() for line in file.readlines()]
 
 
-def select_max_calories(elves_bags: List[int]) -> int:
-    max_calories = 0
-    for bag in elves_bags:
-        if bag >= max_calories:
-            max_calories = bag
-
-    return max_calories
+def should_swith_to_next_elf(line: str) -> bool:
+    return line == ""
 
 
-def select_biggest_calories_bag(input_file: str) -> int:
-    elves_bags = extract_elves_bags(input_file)
-    return select_max_calories(elves_bags)
+def report_calories_in_inventory(elvish_inventory: List[int], calories: int):
+    elvish_inventory.append(calories)
+
+
+def initialize_calories_of_new_elf() -> int:
+    return 0
+
+
+# Output: elvish inventories of calories: [2000, 3000, 1000]
+def create_elvish_inventories(data_lines: List[str]) -> List[int]:
+    elvish_inventories = []
+    calories_of_current_elf = initialize_calories_of_new_elf()
+    for line in data_lines:
+        if should_swith_to_next_elf(line):
+            report_calories_in_inventory(elvish_inventories, calories_of_current_elf)
+            calories_of_current_elf = initialize_calories_of_new_elf()
+        else:
+            calories_of_current_elf += int(line)
+    return elvish_inventories
+
+
+def compute_total_calories_of_top_elves(elvish_inventories: List[int], number_of_elves: int) -> int:
+    elvish_inventories.sort(reverse=True)
+    inventories_with_max_calories = elvish_inventories[0:number_of_elves]
+
+    return sum(inventories_with_max_calories)
 
 
 if __name__ == "__main__":
-    print(select_biggest_calories_bag("./2022/data/day01-input.txt"))
+    # ---- TEST DATA -----
+    test_data = [
+        "1000", "2000", "3000", "",
+        "4000", "",
+        "5000", "6000", "",
+        "7000", "8000", "9000", "",
+        "10000", ""
+    ]
+    # Solution for 1-a & 1-b
+    test_elvish_inventories = create_elvish_inventories(test_data)
+    print(compute_total_calories_of_top_elves(test_elvish_inventories, 1))
+    print(compute_total_calories_of_top_elves(test_elvish_inventories, 3))
+
+
+    # ---- REAL DATA ----
+    data = read_data("./2022/data/day01-input.txt")
+    elvish_inventories = create_elvish_inventories(data)
+
+    # Solution for 1-a
+    print(compute_total_calories_of_top_elves(elvish_inventories, 1)) # 68802
+    # Solution for 1-b
+    print(compute_total_calories_of_top_elves(elvish_inventories, 3)) # 205370
+
+
+# Algorithmic complexity:
+# Time: o(n)  (with n: number of lines) (+ o(m log(m)) with m: number of inventories)
+# Space: o(n) (array)
