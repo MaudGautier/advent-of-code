9f7aec1fff03ce485fdb4092a68e5ce4551b07f2
Maud <maud.j.gautier@gmail.com>
2022-12-06 10:48:44 +0100

    feat(2022): add 6-a solution

---
+++ 2022/src/day06.py
@@ -0,0 +1,43 @@
+def read_data(file_name):
+    with open(file_name, 'r') as file:
+        return file.readlines()[0].strip()
+
+
+def all_characters_differ(sequence, sequence_length):
+    return len(set(list(sequence))) == sequence_length
+
+
+def part_one(signal):
+    sequence_length = 4
+    for sequence_start in range(len(signal) - sequence_length):
+        sequence = signal[sequence_start:sequence_start + sequence_length]
+        if all_characters_differ(sequence, sequence_length):
+            return sequence_start + sequence_length
+
+
+if __name__ == "__main__":
+    # ---- TEST DATA -----
+    test_data_1 = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+    test_data_2 = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+    test_data_3 = "nppdvjthqldpwncqszvftbrmjlhg"
+    test_data_4 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+    test_data_5 = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+    print("-- Tests on test data:")
+    print(part_one(test_data_1) == 7)
+    print(part_one(test_data_2) == 5)
+    print(part_one(test_data_3) == 6)
+    print(part_one(test_data_4) == 10)
+    print(part_one(test_data_5) == 11)
+
+    # ---- REAL DATA ----
+    data = read_data("./2022/data/day06-input.txt")
+
+    # Solution for 2-a
+    print("\n-- Solution for 2-a:")
+    print(part_one(data))
+
+    # Solution for 2-b
+    # print("\n-- Solution for 2-b:")
+    # print(part_two(data))
+
+# TODO: add complexity
