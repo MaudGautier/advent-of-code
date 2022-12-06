478c0f3e0ece878555e6ea3d86f19db79e88eac7
Maud <maud.j.gautier@gmail.com>
2022-12-06 10:32:25 +0100

    feat(2022): add 5-b solution

--- 2022/src/day05.py
+++ 2022/src/day05.py
@@ -43,15 +43,49 @@ def part_one(stacks, instructions):
     return top_items
 
 
+def get_stacks_and_instructions_part_two(lines):
+    stacks = {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: []}
+    instructions = []
+    for line in lines:
+        # Modify only instructions
+        if line.startswith("move"):
+            infos = line.strip().split(" ")
+            nb = int(infos[1])
+            origin_stack = int(infos[3])
+            destination_stack = int(infos[5])
+            instructions.append((origin_stack, destination_stack, nb))
+        elif not line.startswith(" 1"):
+            starts = [x * 4 for x in range((len(line) + 1) // 4)]
+            for start in starts:
+                item = line[1 + start]
+                stack_index = int(start / 4) + 1
+                if item != ' ':
+                    stacks[stack_index].insert(0, item)
+
+    return (stacks, instructions)
+
+
+def part_two(stacks, instructions):
+    # print(stacks)
+    for instruction in instructions:
+        origin_stack = instruction[0]
+        destination_stack = instruction[1]
+        nb_items = instruction[2]
+        items = stacks[origin_stack][-nb_items:]
+        stacks[origin_stack] = stacks[origin_stack][:-nb_items]
+        stacks[destination_stack] += items
+        # print(instruction, "--->", stacks)
+
+    top_items = ''
+    for stack_index in range(1, 10):
+        if len(stacks[stack_index]) >= 1:
+            top_items += stacks[stack_index][-1]
+
+    return top_items
+
+
 if __name__ == "__main__":
     # ---- TEST DATA -----
-    test_stacks = {1: ["Z", "N"], 2: ["M", "C", "D"], 3: ["P"], 4: [], 5: [], 6: [], 7: [], 8: [], 9: []}
-    test_instructions = [
-        (2, 1),  # "move 1 from 2 to 1",
-        (1, 3), (1, 3), (1, 3),  # "move 3 from 1 to 3",
-        (2, 1), (2, 1),  # "move 2 from 2 to 1",
-        (1, 2)  # "move 1 from 1 to 2"
-    ]
     test_lines = [
         "    [D]    ",
         "[N] [C]    ",
@@ -63,22 +97,41 @@ if __name__ == "__main__":
         "move 2 from 2 to 1",
         "move 1 from 1 to 2"]
     print("-- Tests on test data:")
+    test_stacks = {1: ["Z", "N"], 2: ["M", "C", "D"], 3: ["P"], 4: [], 5: [], 6: [], 7: [], 8: [], 9: []}
+    test_instructions = [
+        (2, 1),  # "move 1 from 2 to 1",
+        (1, 3), (1, 3), (1, 3),  # "move 3 from 1 to 3",
+        (2, 1), (2, 1),  # "move 2 from 2 to 1",
+        (1, 2)  # "move 1 from 1 to 2"
+    ]
     (stacks, instructions) = get_stacks_and_instructions(test_lines)
     print(stacks == test_stacks)
     print(instructions == test_instructions)
     print(part_one(stacks, instructions) == "CMZ")
-    # print(part_two(test_data) == 4)
+
+    test_stacks_part_two = {1: ["Z", "N"], 2: ["M", "C", "D"], 3: ["P"], 4: [], 5: [], 6: [], 7: [], 8: [], 9: []}
+    test_instructions_part_two = [
+        (2, 1, 1),  # "move 1 from 2 to 1",
+        (1, 3, 3),  # "move 3 from 1 to 3",
+        (2, 1, 2),  # "move 2 from 2 to 1",
+        (1, 2, 1)  # "move 1 from 1 to 2"
+    ]
+    (stacks_part_two, instructions_part_two) = get_stacks_and_instructions_part_two(test_lines)
+    print(stacks_part_two == test_stacks_part_two)
+    print(instructions_part_two == test_instructions_part_two)
+    print(part_two(stacks_part_two, instructions_part_two) == "MCD")
 
     # ---- REAL DATA ----
     data = read_data("./2022/data/day05-input.txt")
-    (stacks, instructions) = get_stacks_and_instructions(data)
 
     # Solution for 2-a
     print("\n-- Solution for 2-a:")
+    (stacks, instructions) = get_stacks_and_instructions(data)
     print(part_one(stacks, instructions))
 
-    # # Solution for 2-b
-    # print("\n-- Solution for 2-b:")
-    # print(part_two(data))
+    # Solution for 2-b
+    print("\n-- Solution for 2-b:")
+    (stacks_part_two, instructions_part_two) = get_stacks_and_instructions_part_two(data)
+    print(part_two(stacks_part_two, instructions_part_two))
 
 # TODO: add complexity
