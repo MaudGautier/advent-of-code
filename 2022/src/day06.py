235c4b62714e18a605ab2b2a355cf404adf270da
Maud <maud.j.gautier@gmail.com>
2022-12-06 10:56:53 +0100

    feat(2022): add 6-b solution

--- 2022/src/day06.py
+++ 2022/src/day06.py
@@ -7,12 +7,21 @@ def all_characters_differ(sequence, sequence_length):
     return len(set(list(sequence))) == sequence_length
 
 
-def part_one(signal):
-    sequence_length = 4
+def get_sequence_marker_index(signal, sequence_length):
     for sequence_start in range(len(signal) - sequence_length):
         sequence = signal[sequence_start:sequence_start + sequence_length]
         if all_characters_differ(sequence, sequence_length):
             return sequence_start + sequence_length
+
+
+def part_one(signal):
+    sequence_length = 4
+    return get_sequence_marker_index(signal, sequence_length)
+
+
+def part_two(signal):
+    sequence_length = 14
+    return get_sequence_marker_index(signal, sequence_length)
 
 
 if __name__ == "__main__":
@@ -23,11 +32,18 @@ if __name__ == "__main__":
     test_data_4 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
     test_data_5 = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
     print("-- Tests on test data:")
+    print("-- - for part 1:")
     print(part_one(test_data_1) == 7)
     print(part_one(test_data_2) == 5)
     print(part_one(test_data_3) == 6)
     print(part_one(test_data_4) == 10)
     print(part_one(test_data_5) == 11)
+    print("-- - for part 2:")
+    print(part_two(test_data_1) == 19)
+    print(part_two(test_data_2) == 23)
+    print(part_two(test_data_3) == 23)
+    print(part_two(test_data_4) == 29)
+    print(part_two(test_data_5) == 26)
 
     # ---- REAL DATA ----
     data = read_data("./2022/data/day06-input.txt")
@@ -37,7 +53,7 @@ if __name__ == "__main__":
     print(part_one(data))
 
     # Solution for 2-b
-    # print("\n-- Solution for 2-b:")
-    # print(part_two(data))
+    print("\n-- Solution for 2-b:")
+    print(part_two(data))
 
 # TODO: add complexity
