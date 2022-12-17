9b1f008cf1971719799497ba57450657fc50b952
Maud <maud.j.gautier@gmail.com>
2022-12-17 12:04:27 +0100

    feat(2022): add 11-b solution

--- 2022/src/day11.py
+++ 2022/src/day11.py
@@ -19,55 +19,85 @@ def read_data(file_name):
                 "operation": operation,
                 "test_divisor": test_divisor,
                 True: t,
-                False: f
+                False: f,
+                "divisor": divisor,
             }
         return data
 
 
-def part_one(data):
+def run_one_round(monkeys, number_inspected_items, worry_level_manager):
+    for monkey in monkeys:
+        decisions = monkeys[monkey]
+        items = decisions["items"]
+        items_index_to_delete = []
+        for item_index, item in enumerate(items):
+            number_inspected_items[monkey] += 1
+            worry_level = decisions["operation"](item)
+            worry_level = worry_level_manager(worry_level)
+            is_divisible = decisions["test_divisor"](worry_level)
+            to_monkey = decisions[is_divisible]
+            monkeys[to_monkey]["items"].append(worry_level)
+            items_index_to_delete.append(item_index)
+        for item_index_to_delete in sorted(items_index_to_delete, reverse=True):
+            monkeys[monkey]["items"].pop(item_index_to_delete)
+        # print("Monkey", monkey, "inspected", number_inspected_items[monkey], "items")
+
+
+def part_one(monkeys):
     number_inspected_items = {}
-    for monkey in data:
+    for monkey in monkeys:
         number_inspected_items[monkey] = 0
 
-    for round in range(20):
-        for monkey in data:
-            decisions = data[monkey]
-            items = decisions["items"]
-            items_index_to_delete = []
-            for item_index, item in enumerate(items):
-                number_inspected_items[monkey] += 1
-                worry_level = decisions["operation"](item)
-                worry_level = math.floor(worry_level / 3)
-                is_divisible = decisions["test_divisor"](worry_level)
-                to_monkey = decisions[is_divisible]
-                data[to_monkey]["items"].append(worry_level)
-                items_index_to_delete.append(item_index)
-                # print("Monkey", monkey, "inspects item", item, "-> to worry_level", worry_level, "--> throw to", to_monkey,
-                #       "New items:", data[monkey]["items"], "other monkey items:", data[to_monkey]["items"])
-            for item_index_to_delete in sorted(items_index_to_delete, reverse=True):
-                data[monkey]["items"].pop(item_index_to_delete)
-        # print("After round", round, "data:", data)
+    worry_level_manager = (lambda x: math.floor(x / 3))
 
-    print(number_inspected_items)
+    for round in range(20):
+        # print("== After round", round + 1, "==")
+        run_one_round(monkeys, number_inspected_items, worry_level_manager)
+
+    most_inspected_numbers = sorted(number_inspected_items.values())
+    return most_inspected_numbers[-2] * most_inspected_numbers[-1]
+
+
+def compute_least_common_multiplier(monkeys):
+    least_common_multiplier = 1
+    for monkey in monkeys:
+        least_common_multiplier *= monkeys[monkey]["divisor"]
+
+    return least_common_multiplier
+
+
+def part_two(monkeys):
+    number_inspected_items = {}
+    for monkey in monkeys:
+        number_inspected_items[monkey] = 0
+
+    least_common_multiplier = compute_least_common_multiplier(monkeys)
+    worry_level_manager = (lambda x: x % least_common_multiplier)
+
+    for round in range(10000):
+        # print("== After round", round + 1, "==")
+        run_one_round(monkeys, number_inspected_items, worry_level_manager)
+
     most_inspected_numbers = sorted(number_inspected_items.values())
     return most_inspected_numbers[-2] * most_inspected_numbers[-1]
 
 
 if __name__ == "__main__":
     # ---- TEST DATA -----
-    test_data = read_data("./2022/data/day11-test-input.txt")
-    # print(test_data)
+    test_data_1 = read_data("./2022/data/day11-test-input.txt")
+    test_data_2 = read_data("./2022/data/day11-test-input.txt")  # because test_data_1 has been mutated
     print("-- Tests on test data:")
-    print(part_one(test_data) == 10605)
-    # print(part_two(test_data) == ##)
+    print(part_one(test_data_1) == 10605)
+    print(part_two(test_data_2) == 2713310158)
 
-    # # ---- REAL DATA ----
-    data = read_data("./2022/data/day11-input.txt")
+    # ---- REAL DATA ----
+    data_1 = read_data("./2022/data/day11-input.txt")
+    data_2 = read_data("./2022/data/day11-input.txt")  # because data_1 has been mutated
 
     # Solution for part A
     print("\n-- Solution for part A:")
-    print(part_one(data))  # 151312
-    #
-    # # Solution for part B
-    # print("\n-- Solution for part B:")
-    # part_two(data)  # FCJAPJRE
+    print(part_one(data_1))  # 151312
+
+    # Solution for part B
+    print("\n-- Solution for part B:")
+    print(part_two(data_2))  # 51382025916
