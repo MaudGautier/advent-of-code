86760c5c60f6b11173367111d1dc41f28ec667e7
Maud <maud.j.gautier@gmail.com>
2022-12-17 22:38:19 +0100

    feat(2022): add 13-b solution

--- 2022/src/day13.py
+++ 2022/src/day13.py
@@ -1,4 +1,7 @@
-def read_data(file_name):
+from functools import cmp_to_key
+
+
+def read_data_part_one(file_name):
     with open(file_name, 'r') as file:
         lines = [line.strip() for line in file.readlines()]
         pairs = []
@@ -9,6 +12,11 @@ def read_data(file_name):
             pairs.append((first_element, second_element))
 
         return pairs
+
+
+def read_data_part_two(file_name):
+    with open(file_name, 'r') as file:
+        return [eval(line.strip()) for line in file.readlines() if line.strip()]
 
 
 # Logique : Dès que un faux => return Faux, sinon, continue de checker le prochain
@@ -61,9 +69,40 @@ def part_one(pairs):
     return sum(pairs_correctly_ordered)
 
 
+def compare(item1, item2):
+    if is_in_correct_order((item1, item2)):
+        return -1
+    elif not is_in_correct_order((item1, item2)):
+        return 1
+    else:
+        return 0
+
+
+def find_packet_id(packets, searched_packet):
+    for packet_index, packet in enumerate(packets):
+        packet_id = packet_index + 1
+        if packet == searched_packet:
+            return packet_id
+
+
+def part_two(packets):
+    divider_packet_1 = [[2]]
+    divider_packet_2 = [[6]]
+
+    packets.append(divider_packet_1)
+    packets.append(divider_packet_2)
+
+    sorted_packets = sorted(packets, key=cmp_to_key(compare))
+
+    id_1 = find_packet_id(sorted_packets, divider_packet_1)
+    id_2 = find_packet_id(sorted_packets, divider_packet_2)
+
+    return id_1 * id_2
+
+
 if __name__ == "__main__":
     # ---- TEST DATA -----
-    test_data = [
+    test_data_1 = [
         ([1, 1, 3, 1, 1], [1, 1, 5, 1, 1]),
         ([[1], [2, 3, 4]], [[1], 4]),
         ([9], [[8, 7, 6]]),
@@ -73,17 +112,36 @@ if __name__ == "__main__":
         ([[[]]], [[]]),
         ([1, [2, [3, [4, [5, 6, 7]]]], 8, 9], [1, [2, [3, [4, [5, 6, 0]]]], 8, 9])
     ]
+    test_data_2 = [
+        [1, 1, 3, 1, 1],
+        [1, 1, 5, 1, 1],
+        [[1], [2, 3, 4]],
+        [[1], 4],
+        [9],
+        [[8, 7, 6]],
+        [[4, 4], 4, 4],
+        [[4, 4], 4, 4, 4],
+        [7, 7, 7, 7],
+        [7, 7, 7],
+        [],
+        [3],
+        [[[]]],
+        [[]],
+        [1, [2, [3, [4, [5, 6, 7]]]], 8, 9],
+        [1, [2, [3, [4, [5, 6, 0]]]], 8, 9]
+    ]
     print("-- Tests on test data:")
-    print(part_one(test_data) == 13)
-    # print(part_two(test_data) == 29)
+    print(part_one(test_data_1) == 13)
+    print(part_two(test_data_2) == 140)
 
     # ---- REAL DATA ----
-    data = read_data("./2022/data/day13-input.txt")
+    data_1 = read_data_part_one("./2022/data/day13-input.txt")
+    data_2 = read_data_part_two("./2022/data/day13-input.txt")
 
     # Solution for part A
     print("\n-- Solution for part A:")
-    print(part_one(data))  # 5675
-    #
-    # # Solution for part B
-    # print("\n-- Solution for part B:")
-    # print(part_two(data))  # 446
+    print(part_one(data_1))  # 5675
+
+    # Solution for part B
+    print("\n-- Solution for part B:")
+    print(part_two(data_2))  # 20383
