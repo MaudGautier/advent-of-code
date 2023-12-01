e0d60a3a50f966ea8a281071545f4861358c4e93
Maud Gautier <maud.j.gautier@gmail.com>
2023-11-30 22:37:47 -0700

    feat(2023): add 1-b solution

--- 2023/src/day01.py
+++ 2023/src/day01.py
@@ -17,14 +17,66 @@ def get_first_and_last_digits(calibration: str) -> tuple[str, str]:
     return first_digit, last_digit
 
 
+DIGITS = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9
+}
+
+
+def get_first_and_last_digits_with_letters(calibration: str) -> tuple[str, str]:
+    first_digit, last_digit = None, None
+    for idx in range(len(calibration)):
+        character = calibration[idx]
+        # digit case
+        if character.isdigit():
+            if not character.isdigit():
+                continue
+
+            last_digit = character
+
+            if first_digit is None:
+                first_digit = character
+
+            continue
+
+        # letter case
+        for digit_letter in DIGITS:
+            if calibration[idx:idx + len(digit_letter)] != digit_letter:
+                continue
+
+            last_digit = str(DIGITS[digit_letter])
+
+            if first_digit is None:
+                first_digit = str(DIGITS[digit_letter])
+
+    return first_digit, last_digit
+
+
 def combine_digits(first: str, last: str) -> int:
     return int(str(first) + str(last))
 
 
-def part_one(calibrations):
+def part_one(calibrations: list[str]) -> int:
     sum = 0
     for calibration in calibrations:
         first_digit, last_digit = get_first_and_last_digits(calibration)
+        number = combine_digits(first_digit, last_digit)
+        sum += number
+
+    return sum
+
+
+def part_two(calibrations: list[str]) -> int:
+    sum = 0
+    for calibration in calibrations:
+        first_digit, last_digit = get_first_and_last_digits_with_letters(calibration)
         number = combine_digits(first_digit, last_digit)
         sum += number
 
@@ -39,12 +91,26 @@ if __name__ == "__main__":
         "a1b2c3d4e5f",
         "treb7uchet",
     ]
+    test_data_2 = [
+        "two1nine",
+        "eightwothree",
+        "abcone2threexyz",
+        "xtwone3four",
+        "4nineeightseven2",
+        "zoneight234",
+        "7pqrstsixteen",
+    ]
     print("-- Tests on test data:")
     print(part_one(test_data) == 142)
+    print(part_two(test_data_2) == 281)
 
     # ---- REAL DATA ----
     data = read_data("./2023/data/day01-input.txt")
 
     # Solution for part A
     print("\n-- Solution for part A:")
-    print(part_one(data))  #
+    print(part_one(data))  # 54239
+
+    # Solution for part B
+    print("\n-- Solution for part B:")
+    print(part_two(data))  # 55343
