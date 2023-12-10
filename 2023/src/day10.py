bf7b0a0055f8ebe93c78e7c8e8c3607b76045115
Maud Gautier <maud.j.gautier@gmail.com>
2023-12-09 23:37:41 -0700

    feat(2023): add 10-b solution

--- 2023/src/day10.py
+++ 2023/src/day10.py
@@ -79,6 +79,51 @@ def part_one(grid: list[str]) -> int:
     return bfs_farthest_point(grid, start)
 
 
+def bfs_get_visited(grid: list[str], start: Position) -> set[Position]:
+    visited = set()
+    to_visit = deque()  # queue
+    to_visit.append((start, 0))
+    max_depth = 0
+    while len(to_visit) > 0:
+        position, depth = to_visit.popleft()
+        max_depth = max(max_depth, depth)
+        visited.add(position)
+        cell = grid[position[0]][position[1]]
+        neighbors = find_neighbors(grid, position, cell)
+        for neighbor in neighbors:
+            if neighbor in visited:
+                continue
+            to_visit.append((neighbor, depth + 1))
+
+    return visited
+
+
+def count_pipes_north_on_left(position: Position, grid: list[str], pipes) -> int:
+    count = 0
+    for i in range(0, position[1]):
+        # Count J, L and | that correspond to connections to north => know if we are within loop
+        # NB: S is because it counts as a "north" (in my dataset + sample data - but should count OR NOT depending on if
+        # S connects north/south or east/west)
+        if (position[0], i) in pipes and grid[position[0]][i] in ["J", "L", "|", "S"]:
+            count += 1
+    return count
+
+
+def part_two(grid: list[str]) -> int:
+    start = find_start_position(grid)
+    pipes = bfs_get_visited(grid, start)
+    nb_tiles = 0
+    for row_index, row in enumerate(grid):
+        for col_index, cell in enumerate(row):
+            if (row_index, col_index) in pipes:
+                continue
+            nb_north_pipes_left = count_pipes_north_on_left((row_index, col_index), grid, pipes)
+            if nb_north_pipes_left % 2 == 1:
+                nb_tiles += 1
+
+    return nb_tiles
+
+
 if __name__ == "__main__":
     # ---- TEST DATA -----
     test_data_1 = [
@@ -114,7 +159,56 @@ if __name__ == "__main__":
     print(part_one(test_data_2) == 4)
     print(part_one(test_data_3) == 8)
     print(part_one(test_data_4) == 8)
-    # print(part_two(test_data) == 2)
+    test_data_5 = [
+        "...........",
+        ".S-------7.",
+        ".|F-----7|.",
+        ".||.....||.",
+        ".||.....||.",
+        ".|L-7.F-J|.",
+        ".|..|.|..|.",
+        ".L--J.L--J.",
+        "..........."
+    ]
+    test_data_6 = [
+        "..........",
+        ".S------7.",
+        ".|F----7|.",
+        ".||OOOO||.",
+        ".||OOOO||.",
+        ".|L-7F-J|.",
+        ".|II||II|.",
+        ".L--JL--J.",
+        ".........."
+    ]
+    test_data_7 = [
+        ".F----7F7F7F7F-7....",
+        ".|F--7||||||||FJ....",
+        ".||.FJ||||||||L7....",
+        "FJL7L7LJLJ||LJ.L-7..",
+        "L--J.L7...LJS7F-7L7.",
+        "....F-J..F7FJ|L7L7L7",
+        "....L7.F7||L7|.L7L7|",
+        ".....|FJLJ|FJ|F7|.LJ",
+        "....FJL-7.||.||||...",
+        "....L---J.LJ.LJLJ..."
+    ]
+    test_data_8 = [
+        "FF7FSF7F7F7F7F7F---7",
+        "L|LJ||||||||||||F--J",
+        "FL-7LJLJ||||||LJL-77",
+        "F--JF--7||LJLJ7F7FJ-",
+        "L---JF-JLJ.||-FJLJJ7",
+        "|F|F-JF---7F7-L7L|7|",
+        "|FFJF7L7F-JF7|JL---7",
+        "7-L-JL7||F7|L7F-7F7|",
+        "L.L7LFJ|||||FJL7||LJ",
+        "L7JLJL-JLJLJL--JLJ.L"
+    ]
+    print(part_two(test_data_5) == 4)
+    print(part_two(test_data_6) == 4)
+    print(part_two(test_data_7) == 8)
+    print(part_two(test_data_8) == 10)
 
     # ---- REAL DATA ----
     data = read_data("./2023/data/day10-input.txt")
@@ -122,7 +216,7 @@ if __name__ == "__main__":
     # Solution for part A
     print("\n-- Solution for part A:")
     print(part_one(data))  # 7086
-    #
-    # # Solution for part B
-    # print("\n-- Solution for part B:")
-    # print(part_two(data))  # 1066
+
+    # Solution for part B
+    print("\n-- Solution for part B:")
+    print(part_two(data))  # 317
