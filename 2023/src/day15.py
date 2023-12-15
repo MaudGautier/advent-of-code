3eeec3b9020bb1b8a515a1bd9d80c5558be515fa
Maud Gautier <maud.j.gautier@gmail.com>
2023-12-15 15:35:26 -0700

    feat(2023): add 15-b solution

--- 2023/src/day15.py
+++ 2023/src/day15.py
@@ -17,7 +17,6 @@ def run_hash(string: str) -> int:
     return hash
 
 
-
 def part_one(data: str) -> int:
     commands = parse_commands(data)
     total = 0
@@ -26,6 +25,50 @@ def part_one(data: str) -> int:
 
     return total
 
+
+Lens = tuple[str, int]
+Box = list[Lens]
+
+
+def run_hashmap(commands: list[str]) -> list[Box]:
+    boxes = [[] for i in range(256)]
+    for command in commands:
+        if "=" in command:
+            label = command.split("=")[0]
+            focal_length = int(command.split("=")[1])
+            new_lens = (label, focal_length)
+            box_id = run_hash(label)
+            box = boxes[box_id]
+            for i, lens in enumerate(box):
+                if lens[0] == label:
+                    box[i] = (label, focal_length)
+                    break
+            else:
+                boxes[box_id].append(new_lens)
+        elif "-" in command:
+            label = command.split("-")[0]
+            box_id = run_hash(label)
+            box = boxes[box_id]
+            for i, lens in enumerate(box):
+                if lens[0] == label:
+                    del box[i]
+
+    return boxes
+
+
+def compute_focusing_power(boxes: list[Box]) -> int:
+    focusing_power = 0
+    for box_id, box in enumerate(boxes):
+        for lens_id, lens in enumerate(box):
+            focal_length = lens[1]
+            focusing_power += (box_id + 1) * (lens_id + 1) * focal_length
+    return focusing_power
+
+
+def part_two(data: str) -> int:
+    commands = parse_commands(data)
+    boxes = run_hashmap(commands)
+    return compute_focusing_power(boxes)
 
 
 if __name__ == "__main__":
@@ -37,7 +80,7 @@ if __name__ == "__main__":
     print(run_hash("HAS") == 172)
     print(run_hash("HASH") == 52)
     print(part_one(test_data) == 1320)
-    # print(part_two(test_data) == 2)
+    print(part_two(test_data) == 145)
 
     # ---- REAL DATA ----
     data = read_data("./2023/data/day15-input.txt")
@@ -45,7 +88,7 @@ if __name__ == "__main__":
     # Solution for part A
     print("\n-- Solution for part A:")
     print(part_one(data))  # 514025
-    #
-    # # Solution for part B
-    # print("\n-- Solution for part B:")
-    # print(part_two(data))  # 1066
+
+    # Solution for part B
+    print("\n-- Solution for part B:")
+    print(part_two(data))  # 244461
