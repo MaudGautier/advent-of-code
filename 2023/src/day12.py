63012eb3a93fdb891af1659e35dec2c9bb8e6947
Maud Gautier <maud.j.gautier@gmail.com>
2023-12-16 20:41:25 -0700

    feat(2023): add 12-b solution with cache from functools

--- 2023/src/day12.py
+++ 2023/src/day12.py
@@ -1,3 +1,6 @@
+from functools import cache
+
+
 def read_data(file_name):
     with open(file_name, 'r') as file:
         return [line.strip() for line in file.readlines()]
@@ -7,6 +10,14 @@ def parse_line(line: str):
     springs = line.split(" ")[0]
     counts = [int(i) for i in line.split(" ")[1].split(",")]
     return springs, counts
+
+
+def parse_line_part_two(line: str, expansion: int = 0):
+    springs = line.split(" ")[0]
+    counts = [int(i) for i in line.split(" ")[1].split(",")]
+    unfolded_springs = (springs + "?") * expansion + springs
+    unfolded_counts = counts * (expansion + 1)
+    return unfolded_springs, tuple(unfolded_counts)
 
 
 def get_counts(arrangement: str):
@@ -20,24 +31,24 @@ def get_counts(arrangement: str):
             counts.append(current_count)
             current_count = 0
             continue
-    if arrangement[-1] == "#":
+    if len(arrangement) and arrangement[-1] == "#":
         counts.append(current_count)
     return [count for count in counts if count != 0]
 
 
 def backtrack(springs: str, expected_counts: list[int], arrangement: str, arrangements: set[str] = ""):
-    # print("arrangement", arrangement)
+    actual_counts = get_counts(arrangement)[:-1]
+    if actual_counts != expected_counts[:len(actual_counts)]:
+        return
+
     if len(arrangement) == len(springs):
         actual_counts = get_counts(arrangement)
-        # print(arrangement, actual_counts)
         if actual_counts != expected_counts:
             return
         arrangements.add(arrangement)
         return
 
     # Iterate all possible solutions
-    # print("ITERATING")
-    # for i in range(len(arrangement), len(springs)):
     i = len(arrangement)
     if springs[i] == "#" or springs[i] == ".":
         backtrack(springs, expected_counts, arrangement + springs[i], arrangements)
@@ -51,7 +62,6 @@ def backtrack(springs: str, expected_counts: list[int], arrangement: str, arrang
 def count_arrangements(springs: str, expected_counts: list[int]) -> int:
     arrangements = set()
     backtrack(springs, expected_counts, arrangement="", arrangements=arrangements)
-    # print(arrangements)
     return len(arrangements)
 
 
@@ -60,6 +70,56 @@ def part_one(data: list[str]) -> int:
     for line in data:
         springs, counts = parse_line(line)
         total += count_arrangements(springs, counts)
+    return total
+
+
+# Dynamic programming - recursion
+@cache
+def count_nb_valid_arrangements(springs: str, expected_counts: tuple[int]) -> int:
+    # Base case - if no more groups => springs must contain no "#"
+    if len(expected_counts) == 0:
+        return "#" not in springs
+    # Base case - if no more springs => there must be no expected counts
+    if len(springs) == 0:
+        return len(expected_counts) == 0
+
+    # Recursion
+    spring, rest_springs = springs[0], springs[1:]
+
+    if spring == ".":
+        return count_nb_valid_arrangements(rest_springs, expected_counts)
+
+    if spring == "#":
+        expected_count = expected_counts[0]
+        if (
+                # At least expected_count springs left (to create the group of springs)
+                len(springs) >= expected_count
+                # Only "#" or "?" in the next expected_counts springs (to create the group of springs)
+                and (all(char in ["#", "?"] for char in springs[:expected_count]))
+                # Either "." or "?" in the next expected_counts + 1 spring (to terminate the group of springs)
+                # Or spring is finished
+                and (len(springs) == expected_count or springs[expected_count] in [".", "?"])
+        ):
+            return count_nb_valid_arrangements(springs[expected_count + 1:], expected_counts[1:])
+        return 0
+
+    if spring == "?":
+        return (
+                count_nb_valid_arrangements(f".{rest_springs}", expected_counts)  # Case 1: replaced by "."
+                + count_nb_valid_arrangements(f"#{rest_springs}", expected_counts)  # Case 2: replaced by "#"
+        )
+
+    raise ValueError("SHOULD NOT GET HERE")
+
+
+def part_two(data: list[str]) -> int:
+    total = 0
+    for i, line in enumerate(data):
+        print(i, "/", len(data))
+        springs, counts = parse_line_part_two(line, 4)
+        counted = count_nb_valid_arrangements(springs, counts)
+        total += counted
+
     return total
 
 
@@ -84,7 +144,7 @@ if __name__ == "__main__":
     ]
     print("-- Tests on test data:")
     print(part_one(test_data) == 21)
-    # print(part_two(test_data) == 525152)
+    print(part_two(test_data) == 525152)
 
     # ---- REAL DATA ----
     data = read_data("./2023/data/day12-input.txt")
@@ -92,7 +152,11 @@ if __name__ == "__main__":
     # Solution for part A
     print("\n-- Solution for part A:")
     print(part_one(data))  # 6488
-    #
-    # # Solution for part B
-    # print("\n-- Solution for part B:")
-    # print(part_two(data))  # 827009909817
+
+    # Solution for part B
+    print("\n-- Solution for part B:")
+    print(part_two(data))  # 815364548481
+
+    # OBSERVATION on first attempt for optimisation of part B:
+    # At any increase of expansion => multiply by a given number
+    # => We need to have the computation for expansion 0 and expansion 1 -> compute the division, then do this 5 times
