df1b973f673de6a0a96e31224aba038b3b5dce84
Maud Gautier <maud.j.gautier@gmail.com>
2023-12-19 16:11:35 -0700

    feat(2023): add 19-a solution

---
+++ 2023/src/day19.py
@@ -0,0 +1,103 @@
+def read_data(file_name):
+    with open(file_name, 'r') as file:
+        return file.read().strip()
+
+
+RawCondition = str
+LeftOperator = str
+Operand = str
+RightOperator = int
+Condition = tuple[LeftOperator, Operand, RightOperator]
+Action = str
+Workflow = tuple[list[tuple[Condition, Action]], Action]
+Workflows = dict[str, Workflow]
+Part = dict[str, int]
+
+
+def create_condition(raw_condition: RawCondition) -> Condition:
+    return (raw_condition[0], raw_condition[1], int(raw_condition[2:]))
+
+
+def parse_data(data: str) -> tuple[Workflows, list[Part]]:
+    raw_workflows, raw_parts = data.split("\n\n")
+
+    workflows = {}
+    for raw_workflow in raw_workflows.split("\n"):
+        # print("raw_workflow.split({)", raw_workflow, raw_workflow.split("{"))
+        name, raw_workflow_parts = raw_workflow.split("{")
+        conditions_actions = [(c_a.split(":")[0], c_a.split(":")[1]) for c_a in raw_workflow_parts[:-1].split(",")[:-1]]
+        default_action = raw_workflow_parts[:-1].split(",")[-1]
+        processed_conditions_actions = [(create_condition(ca[0]), ca[1]) for ca in conditions_actions]
+        workflows[name] = (processed_conditions_actions, default_action)
+
+    parts = []
+    for raw_part in raw_parts.split("\n"):
+        ratings = raw_part[1:-1].split(",")
+        part = {}
+        for rating in ratings:
+            name, score = rating.split("=")
+            part[name] = int(score)
+        parts.append(part)
+
+    return workflows, parts
+
+
+def process(part: Part, workflows: Workflows, action_name: str = "in") -> Action:
+    # Base case
+    if action_name == "R" or action_name == "A":
+        return action_name
+
+    # Recurse
+    workflow = workflows[action_name]
+    for condition, action in workflow[0]:
+        letter, operator, value = condition
+        if (operator == ">" and part[letter] > value) or (operator == "<" and part[letter] < value):
+            return process(part, workflows, action)
+
+    default_action = workflow[1]
+    return process(part, workflows, default_action)
+
+
+def part_one(data: str) -> int:
+    workflows, parts = parse_data(data)
+    total = 0
+    for part in parts:
+        action = process(part, workflows)
+        if action == "A":
+            total += sum(part.values())
+    return total
+
+
+if __name__ == "__main__":
+    # ---- TEST DATA -----
+    test_data = r"""px{a<2006:qkq,m>2090:A,rfg}
+pv{a>1716:R,A}
+lnx{m>1548:A,A}
+rfg{s<537:gd,x>2440:R,A}
+qs{s>3448:A,lnx}
+qkq{x<1416:A,crn}
+crn{x>2662:A,R}
+in{s<1351:px,qqz}
+qqz{s>2770:qs,m<1801:hdj,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
+
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+{x=2461,m=1339,a=466,s=291}
+{x=2127,m=1623,a=2188,s=1013}"""
+    print("-- Tests on test data:")
+    print(part_one(test_data) == 19114)
+    # print(part_two(test_data) == 51)
+
+    # ---- REAL DATA ----
+    data = read_data("./2023/data/day19-input.txt")
+
+    # Solution for part A
+    print("\n-- Solution for part A:")
+    print(part_one(data))  # 480738
+    #
+    # # Solution for part B
+    # print("\n-- Solution for part B:")
+    # print(part_two(data))  # 6766
